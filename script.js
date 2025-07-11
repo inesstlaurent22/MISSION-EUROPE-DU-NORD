b4c1f2fb2cc11bc9325b238fd32537456799abea
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-07-11 23:20:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.getElementById('transitionButton').addEventListener('click', function (
 
   setTimeout(() => {
     window.location.href = 'rotterdam.html'; // change cette URL si besoin
-  }, 800); // 1000ms = 1s, ajuste selon la durée du GIF
+  }, 950); // 1000ms = 1s, ajuste selon la durée du GIF
 });
