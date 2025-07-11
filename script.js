31e3dc32d614daf4e64763f712ec40c64177fff2
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-07-11 23:14:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.getElementById('transitionButton').addEventListener('click', function () {
-  const baleineOverlay = document.getElementById('transitionVideoContainer');
+  const baleine = document.getElementById('transitionVideoContainer');
 
-  // Affiche le GIF de transition
-  baleineOverlay.style.display = 'block';
+  baleine.style.display = 'block';
 
-  // Attends la durée du GIF avant de rediriger
   setTimeout(() => {
     window.location.href = 'rotterdam.html'; // change cette URL si besoin
   }, 1000); // 1000ms = 1s, ajuste selon la durée du GIF
