d6ae0d11f44d2bd26154d5107025c5a11212c1b6
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-07-11 22:58:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ document.getElementById('transitionButton').addEventListener('click', function (
   const button = this;
   const baleine-overlay = document.getElementById('transitionVideoContainer');
 
-  // Désactive le bouton pour éviter plusieurs clics
-  button.disabled = true;
-  button.style.cursor = 'not-allowed';
-  button.style.opacity = '0.6'; // Optionnel : effet visuel de désactivation
-
   // Affiche le GIF de transition
   baleine-overlay.style.display = 'block';
 
