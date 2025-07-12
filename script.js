30d00b2ec8a631651515219ef1e4636b25464a07
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-07-12 14:31:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,9 @@ transitionButtons.forEach(button => {
     // Affiche la baleine (image ou vidéo)
     baleine.style.display = 'block';
 
-    // Si tu utilises une vraie balise <video> au lieu d'un GIF
-    if (video) {
-      video.playbackRate = 0.5;
-      video.play();
-    }
-
     // Adapter le délai (ex: 4 secondes)
     setTimeout(() => {
       window.location.href = targetURL;
-    }, 4000);
+    }, 1600);
   });
 });
