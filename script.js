0254ba1b6c8f35d1fb449dc958a3740bd68623f3
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-07-12 16:59:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
       // Lance la redirection après 1.6 secondes
       setTimeout(() => {
         window.location.href = target;
-      }, 1400);
+      }, 1200);
     });
   });
 });
